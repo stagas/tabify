da4955ab7705da94158a34eba07189b918ab7a42
George Stagas <gstagas@gmail.com>
2013-02-27 21:49:12 +0200

    rewrite

--- index.js
+++ index.js
@@ -22,70 +22,123 @@ module.exports = tabify
 /**
  * Convert a list to tabs.
  *
- * @param {Element} el 
- * @return {Emitter} tabs
+ * @param {Element} el
+ * @return {Object} tabs
  * @api public
  */
 
 function tabify (el) {
   var links = []
   var targets = []
-  var prev = null
 
-  var emitter = Emitter({})
+  var tabs = {}
+
+  Emitter(tabs)
+
+  /**
+   * Show tab of link `el`.
+   *
+   * @param {Element} el
+   * @api private
+   */
+
+  tabs.show = function (el) {
+    el.classList.add('active')
+    el.__target.classList.remove('hide')
+    tabs.active = el
+    tabs.activeTarget = el.__target
+    tabs.activeTargetId = el.__targetId
+  }
+
+  /**
+   * Hide tab of link `el`.
+   *
+   * @param {Element} el
+   * @api private
+   */
+
+  tabs.hide = function (el) {
+    el.classList.remove('active')
+    el.__target.classList.add('hide')
+  }
+
+  /**
+   * Hide all tabs.
+   *
+   * @api private
+   */
+
+  tabs.hideAll = function () {
+    // change classes for new active
+    links.forEach(function (a) {
+      a.classList.remove('active')
+    })
+    targets.forEach(function (t) {
+      t.classList.add('hide')
+    })
+  }
+
+  /**
+   * Unbind all listeners.
+   *
+   * @api public
+   */
+
+  tabs.unbind = function () {
+    links.forEach(function (a) {
+      console.log('removing listener...')
+      a.removeEventListener('click', tabs.onclick)
+    })
+    tabs.removeAllListeners('change')
+  }
+
+  /**
+   * onclick listener.
+   *
+   * @param {Event} ev [description]
+   * @api private
+   */
+
+  tabs.onclick = function (ev) {
+    // don't do the default (jump to anchor)
+    ev.preventDefault()
+    ev.stopPropagation()
+
+    // only emit if changed
+    if (tabs.active !== this) {
+      tabs.emit('change', this.__targetId, this.__target, this)
+      tabs.show(this)
+    }
+
+    tabs.hideAll()
+    tabs.show(this)
+
+    return false
+  }
 
   // find tab links
   var res = el.querySelectorAll('a')
+  var active = el.querySelectorAll('a.active')[0]
 
+  // make tabs
   for (var i = 0; i < res.length; i++) {
     var a = res[i]
+    var id = a.attributes.href.textContent
+
+    var target = document.getElementById(id.substr(1))
+    if (!target) continue
+
     links.push(a)
-
-    var id = a.attributes.href.textContent.split('#')[1]
-
-    var target = document.getElementById(id)
     targets.push(target)
 
-    a.__targetId = id
     a.__target = target
+    a.__targetId = id
 
-    // make the first one active by default
+    if (active ? a === active : i == 0) tabs.show(a)
+    else tabs.hide(a)
 
-    if (i == 0) {
-      a.classList.add('active')
-      prev = a
-    }
-    else {
-      target.classList.add('hide')
-    }
-
-    // assign onclick handler
-
-    a.onclick = function (ev) {
-      // don't do the default (jump to anchor)
-      ev.preventDefault()
-      ev.stopPropagation()
-
-      // only emit if changed
-      if (prev !== this) {
-        emitter.emit('change', this.__targetId, this.__target, this)
-      }
-      prev = this
-
-      // change classes for new active
-
-      links.forEach(function (a) {
-        a.classList.remove('active')
-      })
-      targets.forEach(function (t) {
-        t.classList.add('hide')
-      })
-      this.classList.add('active')
-      this.__target.classList.remove('hide')
-
-      return false
-    }
+    a.addEventListener('click', tabs.onclick)
   }
 
-  return emitter
+  return tabs
 }
